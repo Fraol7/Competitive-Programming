fe810c51ffb63630b6fefbcf938e5a2cbd16cb5e
Fraol7 <110989746+Fraol7@users.noreply.github.com>
2023-05-05 21:25:41 +0300

    Create maxCoins.py

---
+++ maxCoins.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        ctr = 0
+        piles.sort()
+        piles = piles[::-1]
+        print(piles)
+        l = 1
+        for i in range(len(piles)//3):
+            ctr += piles[l]
+            l += 2
+        return ctr
