f6f7a2fcc6ba938b816d24787a1dc6589098b066
Fraol7 <110989746+Fraol7@users.noreply.github.com>
2023-05-26 18:55:25 +0300

    Create odd_even_list.py

---
+++ odd_even_list.py
@@ -0,0 +1,13 @@
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if(not head): return head
+        odd = head
+        even = head.next
+        evenHead = even
+        while(even and odd and even.next and odd.next):
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = evenHead
+        return head
