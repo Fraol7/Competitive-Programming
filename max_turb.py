3a90460536bbd8a7ace9e5629386ad33681b3d74
Fraol7 <110989746+Fraol7@users.noreply.github.com>
2023-07-06 10:21:13 +0300

    Create max_turb.py

---
+++ max_turb.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        max_size = 1
+        last = arr[0]
+        curr_size = 1
+        last_op = 0
+
+        for n in arr[1:]:
+            if n > last:
+                if last_op != 2:
+                    curr_size = 2
+                else:
+                    curr_size += 1
+                last_op = 1
+            elif n < last:
+                if last_op != 1:
+                    curr_size = 2
+                else:
+                    curr_size += 1
+                last_op = 2
+            else:
+                curr_size = 1
+                last_op = 0
+                      
+            last = n
+            max_size = max(curr_size,max_size)
+
+        return max_size
