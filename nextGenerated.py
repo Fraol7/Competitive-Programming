9f1f4ec143bdede110628511a271d8d7cbcbead6
Fraol7 <110989746+Fraol7@users.noreply.github.com>
2023-07-07 22:05:51 +0300

    Create nextGenerated.py

---
+++ nextGenerated.py
@@ -0,0 +1,17 @@
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        answer = []
+        for i in nums1:
+            j = nums2.index(i)
+            if j+1 < len(nums2):
+                max_val = i
+                for k in range(j+1,len(nums2)):
+                    if max_val < nums2[k]:
+                        answer.append(nums2[k])
+                        max_val = nums2[k]
+                        break
+                if max_val == i:
+                    answer.append(-1)
+            else:
+                answer.append(-1)
+        return answer  
