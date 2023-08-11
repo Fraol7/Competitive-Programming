e1e8873aeba2580522162333fb43ef4307d42966
Fraol7 <110989746+Fraol7@users.noreply.github.com>
2023-08-11 16:23:16 +0300

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -2,11 +2,12 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         self.ans = []
         self.length = len(nums)
+        self.nums = nums
         self.backtrack([],0)
         return self.ans
 
     def backtrack(self,arr,ind):
         if ind < self.length:
             for i in range(ind,self.length):
-                self.backtrack(arr+[nums[i]],nums,i+1)
+                self.backtrack(arr+[self.nums[i]],i+1)
         self.ans.append(arr)
