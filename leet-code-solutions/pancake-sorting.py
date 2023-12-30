8af25734421e0516b6cf5436096ced0f159bfd0c
Fraol7 <110989746+Fraol7@users.noreply.github.com>
2023-12-30 18:42:35 +0300

    Added solution for Pancake Sorting

---
+++ leet-code-solutions/pancake-sorting.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def pancakeSort(self, arr):
+        ans = []
+        end = len(arr)
+        while end > 0:
+            maxi = max(arr[:end])
+            max_index = arr[:end].index(maxi)
+            k = max_index+1
+            if k != end:
+                ans.append(k)
+                arr[:k] = arr[:k][::-1]
+                ans.append(end)
+                arr[:end] = arr[:end][::-1]
+            end -= 1
+        return ans
