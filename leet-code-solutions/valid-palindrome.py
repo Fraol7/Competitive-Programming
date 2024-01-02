1667d228625f7704ebefb33f77a2e84af3d2c667
Fraol7 <110989746+Fraol7@users.noreply.github.com>
2024-01-02 19:10:29 +0300

    Added solution for Valid Palindrome

--- leet-code-solutions/valid-palindrome.py
+++ leet-code-solutions/valid-palindrome.py
@@ -2,13 +2,12 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         _s = ""
         for i in s:
-            if i.isalnum(): 
+            if i.isalnum():
                 _s += i.lower()
-        rev_s = ""
-        end = len(_s) - 1
-        while end >= 0:
-            rev_s += _s[end]
-            end -= 1
-        if rev_s == _s:
-            return True
-        return False
+        left, right = 0, len(_s)-1
+        while left < right:
+            if _s[left] != _s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
