510efb4d44f64dd2e90c8a1f8679954ec6d67418
Fraol7 <110989746+Fraol7@users.noreply.github.com>
2024-02-08 10:29:39 +0300

    Add solution for Replace the Substring for Balanced String

---
+++ leet-code-solutions/leetcode/replace-the-substring-for-balanced-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        refer = Counter(s)
+        each = len(s)//4
+        max_diff = 0
+        for i in 'QWER':
+            if refer[i] - each > 0:
+                max_diff += refer[i] - each
+                refer[i] -= each
+            else:
+                del refer[i]
+        print(refer, max_diff)
+        if max_diff == 0:
+            return 0
+        if max_diff == 1:
+            return 1
+        left, min_win = 0, float('inf')
+        for right in range(len(s)):
+            if s[right] in refer:
+                refer[s[right]] -= 1
+                if refer[s[right]] >= 0:
+                    max_diff -= 1
+            while left < right and max_diff == 0:
+                min_win = min(min_win, right-left+1)
+                if s[left] in refer:
+                    refer[s[left]] += 1
+                    if refer[s[left]] > 0:
+                        max_diff += 1
+                left += 1
+        return min_win
