5c2d10ff93811a8a5dfb9da0fd163957b445a5d0
Fraol Mulugeta <110989746+Fraol7@users.noreply.github.com>
2024-03-04 01:09:41 +0300

    Add solution for N-Queens II

---
+++ leet-code-solutions/leetcode/n-queens-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        result = []
+        board = [["."]*n for i in range(n)]
+
+        def backtrack(row,cols,posDiags,negDiags):
+            if row == n:
+                result.append(["".join(line) for line in board])
+                return
+            for col in range(n):
+                if col not in cols and row+col not in posDiags and row-col not in negDiags:
+                    board[row][col] = "Q"
+                    backtrack(row+1, cols|set([col]), posDiags|set([row+col]), negDiags|set([row-col]))
+                    board[row][col] = "."
+        backtrack(0,set(),set(),set())
+        return len(result)
